b26b7549153296073ad7376c8568f11832df5529
realaicode <realicode@gmail.com>
2016-09-01 20:47:22 +0800

    工作流 于用户权限系统 对接 OK

---
+++ jc-web/src/main/java/com/realaicy/product/jc/common/wf/temp/ApplicantRepository.java
@@ -0,0 +1,7 @@
+package com.realaicy.product.jc.common.wf.temp;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ApplicantRepository extends JpaRepository<Applicant, Long> {
+
+}
---
+++ jc-web/src/main/java/com/realaicy/product/jc/common/wf/temp/ResumeService.java
@@ -0,0 +1,12 @@
+package com.realaicy.product.jc.common.wf.temp;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ResumeService {
+
+    public void storeResume() {
+        System.out.println("Storing resume ...");
+    }
+
+}
